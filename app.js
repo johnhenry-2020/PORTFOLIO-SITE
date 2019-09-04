dfd5ed68651e65591914b3519a7dafc69b6e12ae
JJ <jonjolivette777@aol.com>
2019-09-04 14:57:33 -0500

    added custom styles, finalized routes, updated views

--- app.js
+++ app.js
@@ -1,51 +1,44 @@
-// =================
-// 	dependencies
-// =================
+// imports
 const express = require('express');
-const projects = require('./data.json');
 
+// creating the app
 const app = express();
-// =================
-// 	middleware
-// =================
+
+// ================= app data ===================
+const { projects } = require('./data/data.json');
+
+// ======  templating  =======
 app.set('views', './views');
-// set view engine to pug
 app.set('view engine', 'pug');
-// static route and the express.static method serving the static files located in the public folder
+
+// ============ static content ==========
 app.use('/static', express.static('public'));
 
-// =================
-// 	route handling
-// =================
-// index route, renders the "Home" page with the locals set to data.projects
+// =============== routes =====================
+// index
 app.get('/', (req, res) => {
 	res.render('index', { projects: projects });
 });
-// about route, renders the "About" page
+// about
 app.get('/about', (req, res) => {
 	res.render('about');
 });
-// projects routes, dynamic "project" routes based on the id of the project
-app.get('/projects', (req, res) => {
+// projects
+app.get('/projects/:id', (req, res) => {
+	res.locals.project = projects.filter((proj) => proj.id === req.params.id)[0];
 	res.render('project');
 });
-
-// =================
-// 	error handling
-// =================
+// ========= error handling ==============
 app.use((req, res, next) => {
 	const err = new Error('Not Found');
 	err.status = 404;
-	console.log('404: Route Not Found');
 	next(err);
 });
 app.use((err, req, res, next) => {
 	res.locals.error = err;
+	console.log('404: Route Not Found');
 	res.render('error');
-	console.log(`Here is the source of the problem...${err.stack}`);
 });
 
-// =========
-// server
-// =========
-app.listen(3000, () => console.log('Local Host: Port 3000'));
+// ======= dev server ===============
+app.listen(3000, () => console.log('Running locally on port 3000'));
